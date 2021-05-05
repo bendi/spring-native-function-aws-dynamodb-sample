0788ed9471b04a42b66828a8a8ae33c7c765c2f9
Marek Bedkowski <marek@bedkowski.pl>
2021-05-05 20:27:00 +0200

    Added support for ssl + got it working on with managed DynamoDB.

--- src/main/java/pl/bedkowski/spring/lambda/config/DynamoDBConfig.java
+++ src/main/java/pl/bedkowski/spring/lambda/config/DynamoDBConfig.java
@@ -1,12 +1,11 @@
 package pl.bedkowski.spring.lambda.config;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
@@ -17,15 +16,34 @@ public class DynamoDBConfig {
         this.dynamoDbConfigProps = dynamoDbConfigProps;
     }
 
-    AwsCredentials awsCredentials() {
-        return AwsBasicCredentials.create(dynamoDbConfigProps.getAccesskey(), dynamoDbConfigProps.getSecretkey());
-    }
-    DynamoDbClient dynamoDbClient(AwsCredentials awsCredentials) {
-        return DynamoDbClient.builder().credentialsProvider(() -> awsCredentials).endpointOverride(dynamoDbConfigProps.getEndpoint()).region(dynamoDbConfigProps.getRegion()).build();
+    @Profile("local")
+    class Local {
+
+        AwsCredentials awsCredentials() {
+            return AwsBasicCredentials.create(dynamoDbConfigProps.getAccesskey(), dynamoDbConfigProps.getSecretkey());
+        }
+
+        DynamoDbClient dynamoDbClient(AwsCredentials awsCredentials) {
+            return DynamoDbClient.builder().credentialsProvider(() -> awsCredentials)
+                                 .endpointOverride(dynamoDbConfigProps.getEndpoint())
+                                 .region(dynamoDbConfigProps.getRegion()).build();
+        }
+
+        @Bean
+        DynamoDbEnhancedClient dynamoDbEnhancedClient() {
+            return DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient(awsCredentials())).build();
+        }
     }
 
-    @Bean
-    DynamoDbEnhancedClient dynamoDbEnhancedClient() {
-        return DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient(awsCredentials())).build();
+    @Profile("!local")
+    class Aws {
+        DynamoDbClient dynamoDbClient() {
+            return DynamoDbClient.builder().region(dynamoDbConfigProps.getRegion()).build();
+        }
+
+        @Bean
+        DynamoDbEnhancedClient dynamoDbEnhancedClient() {
+            return DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient()).build();
+        }
     }
 }
